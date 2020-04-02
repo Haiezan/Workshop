0b6d8d0a4b94bd90d2ed2680cfee0faad6de06ab
Haiezan <807544235@qq.com>
2020-04-02 22:12:20 +0800

    增加节点类

---
+++ Workshop/ModelData/Joint.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workshop.ModelData
+{
+    class Joint
+    {
+        public long ID;
+        public long StdFlrID;
+        public double X;
+        public double Y;
+        public double HDiff;
+    }
+}
