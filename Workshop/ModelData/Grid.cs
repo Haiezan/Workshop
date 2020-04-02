2a4bf1d7582ec9ff7d56b6177910d065e7d339d3
Haiezan <807544235@qq.com>
2020-04-02 22:12:26 +0800

    增加网格类

---
+++ Workshop/ModelData/Grid.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rhino.Geometry;
+
+namespace Workshop.ModelData
+{
+    class Grid
+    {
+        public long ID;
+        public long StdFlrID;
+        public long Jt1ID;
+        public long Jt2ID;
+        public long AxidID;
+    }
+}
