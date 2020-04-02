2ce465f7e3f17e8d7e101ff06c9cd0590b846332
Haiezan <807544235@qq.com>
2020-04-02 22:12:35 +0800

    增加梁类

---
+++ Workshop/ModelData/Beam.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workshop.ModelData
+{
+    class Beam
+    {
+        public long ID;  //梁号
+        public long StdFlrID;    //标准层号
+        public long SectID;  //截面编号
+        public long GridID;  //网格编号
+
+
+
+    }
+}
