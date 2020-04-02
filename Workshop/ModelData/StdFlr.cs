3e15b0e9278f7d161e5043b3b46462e6a1013822
Haiezan <807544235@qq.com>
2020-04-02 22:12:08 +0800

    增加标准层类

---
+++ Workshop/ModelData/StdFlr.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workshop.ModelData
+{
+    class StdFlr
+    {
+        public long ID;
+        public long No;
+        public double Height;
+    }
+}
