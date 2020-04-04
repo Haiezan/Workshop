9789ea42507ac9ec8f28e95f207675c6d3db3746
Haiezan <807544235@qq.com>
2020-04-04 15:19:13 +0800

    增加柱截面类

---
+++ Workshop/ModelData/ColSect.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rhino.Geometry;
+
+namespace Workshop.ModelData
+{
+    class ColSect
+    {
+        public long ID;
+        public long No;
+        public string Name;
+        public int Mat;
+        public int Kind;
+        public double B;
+        public double H;
+        public double U;
+        public double T;
+        public double D;
+        public double F;
+
+        public PolylineCurve curve;
+    }
+}
