09e63faaf5c356b5ee77bcb99a4c1a7a8eb7bb2d
Haiezan <807544235@qq.com>
2020-04-06 19:15:57 +0800

    增加图标

--- Workshop/GhReadYJK.cs
+++ Workshop/GhReadYJK.cs
@@ -123,7 +123,7 @@ namespace Workshop
             {
                 //You can add image files to your project resources and access them like this:
                 // return Resources.IconForThisComponent;
-                return null;
+                return Properties.Resources.readYJK;
             }
         }
 
